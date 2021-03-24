137a7ce6c63bad34aa3095a292f7401d19f4e7b8
Samuel Gonzalez <xame2005@gmail.com>
2021-03-23 23:17:46 -0600

    [CREATED] Footer css

--- src/components/SkFooter.vue
+++ src/components/SkFooter.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="grid grid-col-2">
-    <div>Skimo 2006</div>
-    <div>Visítanos en redes sociales @Skimo</div>
+  <footer>
+    <div class="grid grid-cols-2 text-gray-600 flex items-strech">
+      <div class="grid cols-span-1 p-6">Skimo 2006</div>
+      <div>Visítanos en redes sociales @Skimo</div>
+    </div>
   </footer>
 </template>
